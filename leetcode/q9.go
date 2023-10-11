1787424b4c6605623326dc00562fa13e0d9b0f13
enjoy <172197763@qq.com>
2023-10-11 09:06:30 +0800

    优先队列完成

--- leetcode/q9.go
+++ leetcode/q9.go
@@ -1,80 +1,27 @@
 package leetcode
 
+import "strconv"
+
 //https://leetcode.cn/problems/stock-price-fluctuation/description/?envType=daily-question&envId=2023-10-08
 type StockPrice struct {
-	curIdx     int
-	priceMap   map[int]int
-	valLink    *link
-	valMaxLink *link
-}
-type link struct {
-	idx  int
-	val  int
-	next *link
+	curIdx   int
+	priceMap map[int]int
+	maxHeap  heap
+	minHeap  heap
 }
 
 func Constructor9() StockPrice {
-	obj := StockPrice{0, make(map[int]int), nil, nil}
+	obj := StockPrice{0, make(map[int]int), HeapConstructor("desc"), HeapConstructor("")}
 	return obj
 }
 
 func (this *StockPrice) Update(timestamp int, price int) {
-	_, ok := this.priceMap[timestamp]
-	if ok {
-		this.DelLinkItem(timestamp)
-	}
 	if timestamp > this.curIdx {
 		this.curIdx = timestamp
 	}
-	this.AddLinkItem(timestamp, price)
+	this.maxHeap.HeapPut(price, strconv.Itoa(timestamp))
+	this.minHeap.HeapPut(price, strconv.Itoa(timestamp))
 	this.priceMap[timestamp] = price
-}
-func (this *StockPrice) DelLinkItem(timestamp int) {
-	tmpLink := this.valLink
-	if tmpLink.idx == timestamp {
-		this.valLink = tmpLink.next
-		return
-	}
-	for true {
-		if tmpLink.next.idx == timestamp {
-			if tmpLink.next.next == nil {
-				tmpLink.next = nil
-				this.valMaxLink = tmpLink
-			} else {
-				tmpLink.next = tmpLink.next.next
-			}
-			return
-		}
-		tmpLink = tmpLink.next
-	}
-}
-func (this *StockPrice) AddLinkItem(timestamp int, price int) {
-	tmp := link{timestamp, price, nil}
-	if this.valLink == nil {
-		this.valLink = &tmp
-		this.valMaxLink = &tmp
-		return
-	}
-	tmplink := this.valLink
-	if tmplink.val > price {
-		tmp.next = tmplink
-		this.valLink = &tmp
-		return
-	}
-	for true {
-		if tmplink.next == nil {
-			tmplink.next = &tmp
-			this.valMaxLink = &tmp
-			return
-		}
-		if tmplink.next.val > price {
-			tmp.next = tmplink.next
-			tmplink.next = &tmp
-			return
-		}
-		tmplink = tmplink.next
-	}
-
 }
 
 func (this *StockPrice) Current() int {
@@ -82,11 +29,29 @@ func (this *StockPrice) Current() int {
 }
 
 func (this *StockPrice) Maximum() int {
-	return this.valMaxLink.val
+	for true {
+		k, v := this.maxHeap.HeapRootGet()
+		newk, _ := strconv.Atoi(k)
+		if this.priceMap[newk] == v {
+			return v
+		} else {
+			this.maxHeap.HeapPop()
+		}
+	}
+	return 0
 }
 
 func (this *StockPrice) Minimum() int {
-	return this.valLink.val
+	for true {
+		k, v := this.minHeap.HeapRootGet()
+		newk, _ := strconv.Atoi(k)
+		if this.priceMap[newk] == v {
+			return v
+		} else {
+			this.minHeap.HeapPop()
+		}
+	}
+	return 0
 }
 
 /**
@@ -97,3 +62,129 @@ func (this *StockPrice) Minimum() int {
  * param_3 := obj.Maximum();
  * param_4 := obj.Minimum();
  */
+type heap struct {
+	arr   []kv
+	order string //大根堆desc 小根堆asc
+}
+type kv struct {
+	val int
+	key string
+}
+
+//新增元素
+func (this *heap) HeapPut(num int, key string) {
+	this.arr = append(this.arr, kv{num, key})
+	this.heapUpChange(len(this.arr) - 1)
+}
+
+//弹出根节点
+func (this *heap) HeapPop() (string, int) {
+	k := this.arr[0].key
+	v := this.arr[0].val
+	this.HeapDel(0)
+	return k, v
+}
+
+//获取根节点
+func (this *heap) HeapRootGet() (string, int) {
+	k := this.arr[0].key
+	v := this.arr[0].val
+	return k, v
+}
+
+//删除元素
+func (this *heap) HeapDel(idx int) {
+	if idx >= len(this.arr) || idx < 0 {
+		return
+	}
+	if len(this.arr) == 1 {
+		this.arr = make([]kv, 0)
+		return
+	}
+	this.arr[idx] = this.arr[len(this.arr)-1] //末尾元素取代删除值
+	this.arr = append(this.arr[:len(this.arr)-1])
+	if len(this.arr) > 1 {
+		this.heapDownChange(idx)
+	}
+}
+func pow(num int, pow int) int {
+	if pow == 0 {
+		return 1
+	}
+	for pow == 0 {
+		num *= num
+		pow--
+	}
+	return num
+}
+
+//元素下更改触发调整
+func (this *heap) heapDownChange(idx int) {
+	if len(this.arr)-1 < idx*2+1 {
+		return
+	}
+	left := idx*2 + 1
+	right := left + 1
+	min := left
+	max := right
+	//检查节点下的子节点
+	if len(this.arr) > right {
+		if this.arr[left].val > this.arr[right].val {
+			min = right
+			max = left
+		}
+	} else {
+		max = left
+	}
+	if this.order == "asc" {
+		if this.arr[min].val < this.arr[idx].val {
+			tmp := this.arr[min]
+			this.arr[min] = this.arr[idx]
+			this.arr[idx] = tmp
+			this.heapDownChange(min)
+		}
+	} else {
+		if this.arr[max].val > this.arr[idx].val {
+			tmp := this.arr[max]
+			this.arr[max] = this.arr[idx]
+			this.arr[idx] = tmp
+			this.heapDownChange(max)
+		}
+	}
+	return
+}
+
+//元素上更改触发调整
+func (this *heap) heapUpChange(idx int) {
+	p := (idx - 1) / 2
+	if this.order == "asc" {
+		if this.arr[p].val > this.arr[idx].val { //比父节点小则交换&继续检测
+			tmp := this.arr[p]
+			this.arr[p] = this.arr[idx]
+			this.arr[idx] = tmp
+			if p == 0 {
+				return
+			}
+			this.heapUpChange(p)
+		}
+	} else {
+		if this.arr[p].val < this.arr[idx].val { //比父节点大则交换&继续检测
+			tmp := this.arr[p]
+			this.arr[p] = this.arr[idx]
+			this.arr[idx] = tmp
+			if p == 0 {
+				return
+			}
+			this.heapUpChange(p)
+		}
+	}
+	return
+}
+
+//order asc小根堆 desc大根堆
+func HeapConstructor(order string) heap {
+	if order != "desc" {
+		order = "asc"
+	}
+	return heap{make([]kv, 0), order}
+}
--- main.go
+++ main.go
@@ -6,6 +6,30 @@ import (
 )
 
 func main() {
+	tmp := 0
+	obj := leetcode.Constructor9()
+	// fun := []string{"StockPrice", "update", "maximum", "current", "minimum", "update", "update", "maximum", "current", "current", "minimum", "minimum", "maximum", "current", "maximum", "current", "maximum", "update", "minimum", "current", "current", "maximum", "update", "minimum", "maximum", "minimum", "minimum", "update", "maximum", "update", "maximum", "current", "current", "maximum", "maximum", "minimum", "update", "update", "maximum", "update", "current", "minimum", "minimum", "maximum", "minimum", "maximum", "update", "update", "minimum", "update", "current", "maximum"}
+	// par := [][]int{{}, {63, 7626}, {}, {}, {}, {87, 9207}, {47, 8547}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {93, 4215}, {}, {}, {}, {}, {87, 2453}, {}, {}, {}, {}, {65, 9645}, {}, {44, 7696}, {}, {}, {}, {}, {}, {}, {70, 2959}, {33, 393}, {}, {77, 7333}, {}, {}, {}, {}, {}, {}, {62, 7696}, {29, 3348}, {}, {75, 3348}, {}, {}}
+	fun := []string{"StockPrice", "update", "update", "current", "maximum", "update", "maximum", "update", "minimum"}
+	par := [][]int{{}, {1, 10}, {2, 5}, {}, {}, {1, 3}, {}, {4, 2}, {}}
+	for k, v := range fun {
+		if v == "update" {
+			obj.Update(par[k][0], par[k][1])
+		}
+		if v == "current" {
+			tmp = obj.Current()
+		}
+		if v == "maximum" {
+			tmp = obj.Maximum()
+		}
+		if v == "minimum" {
+			tmp = obj.Minimum()
+		}
+		fmt.Println(tmp)
+		// if tmp == 2453 {
+		// 	fmt.Println(2453)
+		// }
+	}
 	// obj := leetcode.Constructor8()
 	// param := [7]int{100, 80, 60, 70, 60, 75, 85}
 	// for _, v := range param {
@@ -13,5 +37,16 @@ func main() {
 	// 	fmt.Println(param_1)
 	// }
 	// fmt.Println(leetcode.SplitNum(687))
-	fmt.Println(leetcode.SumDistance([]int{-10, -13, 10, 14, 11}, "LRLLR", 2))
+	// fmt.Println(leetcode.SumDistance([]int{-10, -13, 10, 14, 11}, "LRLLR", 2))
+	// obj := datastruct.HeapConstructor("desc")
+	// obj.HeapPut(4, "444")
+	// obj.HeapPut(1, "555")
+	// obj.HeapPut(6, "666")
+	// obj.HeapPut(7, "777")
+	// obj.HeapPut(8, "888")
+	// obj.HeapPut(9, "999")
+	// obj.HeapPut(10, "1010")
+	// k, v := obj.HeapPop()
+	// fmt.Println(k, v)
+	// obj.HeapShow()
 }
