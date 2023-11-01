fc3c9b99b30ead31ccb6f090e4239ea7a355bf01
wyz <wuyizhuang@qdlauncher.com>
2023-11-01 14:17:19 +0800

    未解决

---
+++ leetcode/q29.go
@@ -0,0 +1,6 @@
+package leetcode
+
+// https://leetcode.cn/problems/maximum-employees-to-be-invited-to-a-meeting/discussion/?envType=daily-question&envId=2023-11-01
+func maximumInvitations(favorite []int) int {
+	return 0
+}
