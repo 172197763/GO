15ed828023909d21ea13eaf0908318d7ef253065
wyz <wuyizhuang@qdlauncher.com>
2023-11-08 09:36:24 +0800

    完成

--- leetcode/q31.go
+++ leetcode/q31.go
@@ -10,35 +10,36 @@ package leetcode
  *     Next *Node
  * }
  */
-type Node31 struct {
+type Node struct {
 	Val   int
-	Left  *Node31
-	Right *Node31
-	Next  *Node31
+	Left  *Node
+	Right *Node
+	Next  *Node
 }
 
-func connect(root *Node31) *Node31 {
-	// if root == nil || (root.Left == nil && root.Right == nil) {
-	// 	return root
-	// }
-	// parent := []*Node{root}
-	// for true {
-	// 	child := []*Node{}
-	// 	for k, v := range parent {
-	// 		if k > 0 {
-	// 			parent[k-1].Next = parent[k]
-	// 		}
-	// 		if v.Left != nil {
-	// 			child = append(child, v.Left)
-	// 		}
-	// 		if v.Right != nil {
-	// 			child = append(child, v.Right)
-	// 		}
-	// 	}
-	// 	if len(child) <= 1 {
-	// 		break
-	// 	}
-	// 	copy(parent, child)
-	// }
+func Connect(root *Node) *Node {
+	if root == nil || (root.Left == nil && root.Right == nil) {
+		return root
+	}
+	parent := []*Node{root}
+	for true {
+		child := []*Node{}
+		for k, v := range parent {
+			if k > 0 {
+				parent[k-1].Next = parent[k]
+			}
+			if v.Left != nil {
+				child = append(child, v.Left)
+			}
+			if v.Right != nil {
+				child = append(child, v.Right)
+			}
+		}
+		if len(child) <= 1 {
+			break
+		}
+		parent = make([]*Node, len(child))
+		copy(parent, child)
+	}
 	return root
 }
